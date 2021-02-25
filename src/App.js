f8afcb9e056752328d7d8374faf783fb43e3198c
MP <princi8@gmail.comm>
2021-02-25 15:37:43 +0100

    Iteration 1
    Working on the next ones, and need to improve styling

--- src/App.js
+++ src/App.js
@@ -21,27 +21,48 @@ class App extends React.Component {
   render() {
   return(
     <div classname= "App">
-      <h3>IronContacts List</h3>
-      <table  cellpadding="2">
-<tbody>
-<tr>
-<td>Picture</td>
-<td>Name</td>
-<td>Popularity</td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>{this.state.c.slice(0,5).map(a=>(<td><img src={a.pictureUrl} width="60" height="90"></img></td>))}</td>
-<td>{this.state.c.slice(0,5).map(a=>( <td>{a.name}</td>))}</td>
-<td>{this.state.c.slice(0,5).map(a=>( <td>{a.popularity}</td>))}</td>
-<td>&nbsp;</td>
-</tr>
-</tbody>
-</table>
+      <h3 style={{ display: 'flex', justifyContent: 'center' }}>IronContacts List</h3>
 
+      {/* <table  cellpadding="2">
+        <tbody>
+        <tr>
+        <td>Picture</td>
+        <td>Name</td>
+        <td>Popularity</td>
+        </tr>
+        <tr>
+        <td>&nbsp;</td>
+        <td>{this.state.c.slice(0,5).map(a=>(<td><img src={a.pictureUrl} width="60" height="90"></img></td> ))}</td>
+        <td>{this.state.c.slice(0,5).map(a=>( <td>{a.name}</td>))}</td>
+        <td>{this.state.c.slice(0,5).map(a=>( <td>{a.popularity}</td>))}</td>
+        <td>&nbsp;</td>
+        </tr>
+        </tbody>
+        </table> */}
 
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <table >
+            <thead >
+              <tr>
+                <th >Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+              </tr>
+            </thead>
 
-    
+            <tbody>
+              {this.state.c.slice(0,5).map(contact => {
+                return (
+                  <tr >
+                    <td><img src={contact.pictureUrl} height='90px'alt={contact.name}/> </td>
+                    <td >{contact.name}</td>
+                    <td>{contact.popularity}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+    </div>
     </div>
   );
 }}
